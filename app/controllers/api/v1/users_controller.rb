dc07d3b319080758d428dc561a5dcf4c2c81d050
abel-tefera <abeltefera16@gmail.com>
2023-09-22 13:02:54 +0300

    Linters fix

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,26 +1,26 @@
-class Api::V1::UsersController < ApplicationController  
-    def new_session
-      return unless request.content_type == 'application/json'
-  
-      user = User.where(user_name: user_params[:user_name])
-      if user.empty?
-        new_user = User.create(user_params)
-        msg = {
-          id: new_user.id,
-          user_name: new_user.user_name,
-          message: 'user created'
-        }
-      else
-        msg = {
-          id: user[0].user_name,
-          user_name: user[0].user_name,
-          message: 'success'
-        }
-      end
-      render json: msg, status: :ok
+class Api::V1::UsersController < ApplicationController
+  def new_session
+    return unless request.content_type == 'application/json'
+
+    user = User.where(user_name: user_params[:user_name])
+    if user.empty?
+      new_user = User.create(user_params)
+      msg = {
+        id: new_user.id,
+        user_name: new_user.user_name,
+        message: 'user created'
+      }
+    else
+      msg = {
+        id: user[0].user_name,
+        user_name: user[0].user_name,
+        message: 'success'
+      }
     end
-  
-    def user_params
-      params.require(:user).permit(:user_name)
-    end
+    render json: msg, status: :ok
   end
+
+  def user_params
+    params.require(:user).permit(:user_name)
+  end
+end
