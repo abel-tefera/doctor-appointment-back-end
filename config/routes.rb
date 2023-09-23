d623bac1ea7d04dbeaa64b902f4c36f8fc99aa86
momo-87 <momochristian87@gmail.com>
2023-09-23 13:30:13 +0100

    Mergin authentication branch into doctor-data branch

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,8 @@
 Rails.application.routes.draw do
   namespace :api do
-    resources :doctors, only: [:index, :create]
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-
-  namespace :api do
     namespace :v1 do
       # resources :users
+      resources :doctors, only: [:index]
       post 'users/new_session', to: 'users#new_session'
     end
   end
