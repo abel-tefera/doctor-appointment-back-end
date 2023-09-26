0598d27c08224f463dc40570a8da0ee51c8b450c
momo-87 <momochristian87@gmail.com>
2023-09-26 11:06:16 +0100

    Create API end point to randomly select three doctors for the main frontend page

--- app/controllers/api/v1/doctors_controller.rb
+++ app/controllers/api/v1/doctors_controller.rb
@@ -1,20 +1,33 @@
 class Api::V1::DoctorsController < ApplicationController
   def index
-    doctors = Doctor.pluck(:name, :bio, :hospital, :specialization, :rate,
+    all_doctors = Doctor.pluck(:name, :bio, :hospital, :specialization, :rate,
                            :image).map do |name, bio, hospital, specialization, rate, image|
       {name:, bio:, hospital:, specialization:, rate:,
         image: Base64.encode64(image) }
     end
+    render json: all_doctors
+  end
+
+  def three_first_doctors
+    all_doctors = Doctor.pluck(:name, :bio, :hospital, :specialization, :rate,
+                          :image).map do |name, bio, hospital, specialization, rate, image|
+      {name:, bio:, hospital:, specialization:, rate:,
+        image: Base64.encode64(image) }
+    end
+    doctors = all_doctors.shuffle.sample(3)
     render json: doctors
   end
 
-  def show
-    doctor = Doctor.find(params[:id])
-    encoded_image = Base64.encode64(doctor.image)
-    doctor.image = encoded_image
 
-    render json: doctor
-  end
+
+
+  # def show
+  #   doctor = Doctor.find(params[:id])
+  #   encoded_image = Base64.encode64(doctor.image)
+  #   doctor.image = encoded_image
+
+  #   render json: doctor
+  # end
 
   private
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   namespace :api do   
     namespace :v1 do
-      resources :doctors, only: [:index, :show]
+      resources :doctors, only: [:index]
       # resources :users
+      get 'doctors/main', to: 'doctors#three_first_doctors'
       post 'users/new_session', to: 'users#new_session'
     end
   end
