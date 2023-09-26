a9883181c92815f3c466ea2c288443b57b13277f
momo-87 <momochristian87@gmail.com>
2023-09-26 14:36:33 +0100

    Fix linter errors

--- app/controllers/api/v1/doctors_controller.rb
+++ app/controllers/api/v1/doctors_controller.rb
@@ -1,8 +1,8 @@
 class Api::V1::DoctorsController < ApplicationController
   def index
-    all_doctors = Doctor.pluck(:name, :bio, :hospital, :specialization, :rate,
-                           :image).map do |name, bio, hospital, specialization, rate, image|
-      {name:, bio:, hospital:, specialization:, rate:,
+    all_doctors = Doctor.pluck(:id, :name, :bio, :hospital, :specialization, :rate,
+                               :image).map do |id, name, bio, hospital, specialization, rate, image|
+      { id:, name:, bio:, hospital:, specialization:, rate:,
         image: Base64.encode64(image) }
     end
     render json: all_doctors
@@ -10,13 +10,14 @@ class Api::V1::DoctorsController < ApplicationController
 
   def three_first_doctors
     all_doctors = Doctor.pluck(:id, :name, :bio, :hospital, :specialization, :rate,
-                          :image).map do |id, name, bio, hospital, specialization, rate, image|
-      {id:, name:, bio:, hospital:, specialization:, rate:,
+                               :image).map do |id, name, bio, hospital, specialization, rate, image|
+      { id:, name:, bio:, hospital:, specialization:, rate:,
         image: Base64.encode64(image) }
     end
     doctors = all_doctors.shuffle.sample(3)
     render json: doctors
   end
+
   private
 
   def doctor_params
