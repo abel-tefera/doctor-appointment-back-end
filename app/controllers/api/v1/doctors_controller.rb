88db7092c7dfd40d86ecc9e38b49bade6ad08a99
momo-87 <momochristian87@gmail.com>
2023-09-26 13:49:55 +0100

    remove commented code

--- app/controllers/api/v1/doctors_controller.rb
+++ app/controllers/api/v1/doctors_controller.rb
@@ -9,26 +9,14 @@ class Api::V1::DoctorsController < ApplicationController
   end
 
   def three_first_doctors
-    all_doctors = Doctor.pluck(:name, :bio, :hospital, :specialization, :rate,
-                          :image).map do |name, bio, hospital, specialization, rate, image|
-      {name:, bio:, hospital:, specialization:, rate:,
+    all_doctors = Doctor.pluck(:id, :name, :bio, :hospital, :specialization, :rate,
+                          :image).map do |id, name, bio, hospital, specialization, rate, image|
+      {id:, name:, bio:, hospital:, specialization:, rate:,
         image: Base64.encode64(image) }
     end
     doctors = all_doctors.shuffle.sample(3)
     render json: doctors
   end
-
-
-
-
-  # def show
-  #   doctor = Doctor.find(params[:id])
-  #   encoded_image = Base64.encode64(doctor.image)
-  #   doctor.image = encoded_image
-
-  #   render json: doctor
-  # end
-
   private
 
   def doctor_params
