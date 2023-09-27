accd695fd225c6d5b0fea59253aba87225f76e2a
inoulen inoulen <inoulenkon@gmail.com>
2023-09-27 13:21:48 +0530

    Add image attribute as a binary

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,7 +1,8 @@
 class Doctor < ApplicationRecord
   has_many :appointments, dependent: :destroy
   belongs_to :user
-
+  attribute :image, :binary
+  
   validates :name, presence: true
   validates :bio, presence: true
   validates :hospital, presence: true
