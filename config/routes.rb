1c273f4175db898efb1583385d0a41a8f4b16ba4
inoulen inoulen <inoulenkon@gmail.com>
2023-09-27 15:56:54 +0530

    Update routes to add resources for create doctors

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do   
     namespace :v1 do
-      resources :doctors, only: [:index]
+      resources :doctors, only: [:index, :create]
       # resources :users
       get 'doctors/main', to: 'doctors#three_first_doctors'
       resources :appointments, only: [:create]
