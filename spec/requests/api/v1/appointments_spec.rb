51c012689682c939e5290eab00424944f8586b0e
ragan <raganalemu3@gmail.com>
2023-09-29 14:05:17 +0300

    Add spec for appointments

---
+++ spec/requests/api/v1/appointments_spec.rb
@@ -0,0 +1,38 @@
+require 'swagger_helper'
+
+RSpec.describe 'api/v1/appointments', type: :request do
+
+  path '/api/v1/appointments' do
+
+    post('create appointment') do
+      response(200, 'successful') do
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+  end
+
+  path '/api/v1/appointments/find_all' do
+
+    post('find_all appointment') do
+      response(200, 'successful') do
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+  end
+end
